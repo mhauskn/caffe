3d73bea42c3864f0c03b064718b071e3316a4c5c
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2014-11-05 17:05:41 -0600

    Added small python script for loading network.

---
+++ load_net.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+caffe_root = './'
+sys.path.insert(0, caffe_root + 'python')
+import caffe
+
+if len(sys.argv) < 3:
+    print 'usage:', sys.argv[0], 'net.proto net.caffemodel'
+    exit()
+
+net = caffe.Net(sys.argv[1], sys.argv[2])
+net.set_phase_train()
+net.set_mode_cpu()
+
+def showInputImage(num=0, chan=0):
+    plt.imshow(net.blobs['data'].data[num][chan])
+    plt.show()
+
+def showInputImageActivation(num=0):
+    plt.imshow(net.blobs['data'].data[num][0])
+    plt.annotate(str(net.blobs['ip2'].data[num]),
+                 xy=(0,0), xytext=(.8,0), fontsize=20)
+    plt.show()
+
+def SaveImagesAndActivations():
+    net.forward()
+    for i in range(net.blobs['data'].data.shape[0]):
+        plt.imshow(net.blobs['data'].data[i][0])
+        plt.annotate(str(net.blobs['ip2'].data[i]),
+                     xy=(0,0), xytext=(.8,0), fontsize=20)
+        plt.savefig('figs/%d.pdf'%i)
+        plt.clf()
+
+def findNonzeroLabel():
+    while not np.nonzero(net.blobs['label'].data)[0]:
+        print 'all zero'
+        net.forward()
